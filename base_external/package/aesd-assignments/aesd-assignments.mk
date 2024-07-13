9a8d50bf844300d3407a82cf5490ba11001a7b45
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-07-13 10:47:46 +0530

    fixes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -3,20 +3,18 @@
 # AESD-ASSIGNMENTS
 #
 ##############################################################
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9cfd3623e28765953e8f14fc8a875bb05bce8d52 
-# Note: Be sure to reference the ssh repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-SshgurkiratSingh.git'
+AESD_ASSIGNMENTS_VERSION = ad7fe99dfc2ffb2678b5858c3844ff077529112e
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-SshgurkiratSingh.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
+
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0755 -d $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+    $(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/finder-app/conf/
     $(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
     $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
     $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
@@ -24,7 +22,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-
 endef
-$(eval $(generic-package))
 
+$(eval $(generic-package))
