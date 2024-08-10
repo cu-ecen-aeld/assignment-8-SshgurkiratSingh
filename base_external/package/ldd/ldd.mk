f486e6d5aed66a3922563a446ff700a8de95c25c
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-08-10 11:34:56 +0530

    added

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,20 @@
+LDD_VERSION = 'cb598601eda3c48ccbfc2d0a6d85de75acb58737'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-SshgurkiratSingh.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = scull misc-modules
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(generic-package))
+$(eval $(kernel-module))
