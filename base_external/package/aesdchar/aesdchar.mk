02b4ba28213f1812b187c584db18d94b7d15d33c
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-10-07 20:13:02 +0530

    Automated Push

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,11 +1,11 @@
-LDD_VERSION = '86d93ce808f9b65e83f844843aa1caa5f0dd17c4'
+AESDCHAR_VERSION = '86d93ce808f9b65e83f844843aa1caa5f0dd17c4'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-SshgurkiratSingh.git'
-LDD_SITE_METHOD = git
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-SshgurkiratSingh.git'
+AESDCHAR_SITE_METHOD = git
 
-LDD_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 #misc_modules scull
 
 $(eval $(kernel-module))
